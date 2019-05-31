35d46b9f272cc91c51c7b831bb60b1a453bfca96
line8847 <line8847@CA-SU-F106-12.sduhsd.lan>
2019-05-31 08:48:53 -0700

    finish events project

--- src/JavaProject/RunMe.java
+++ src/JavaProject/RunMe.java
@@ -16,12 +16,12 @@ import java.util.Scanner;
  *
  * @author line8847
  */
-public class JFrame extends javax.swing.JFrame{
+public class RunMe extends javax.swing.JFrame{
     UserList u;
     /**
      * Creates new form JFrame
      */
-    public JFrame() throws IOException{
+    public RunMe() throws IOException{
 
         u = new UserList();
         initComponents();
@@ -195,21 +195,22 @@ public class JFrame extends javax.swing.JFrame{
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RunMe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RunMe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RunMe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RunMe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run(){
                 try{
-                    new JFrame().setVisible(true);
+                    new RunMe().setVisible(true);
                 }
                 catch (IOException e){
                     System.out.println(e);
